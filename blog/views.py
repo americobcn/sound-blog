52c6d97b0d1a2a6099a30c8653f6d0aa0c1ea2d7
americobcn <americo.cot@gmail.com>
2025-02-23 14:36:13 +0100

    Start refactoring templates

--- blog/views.py
+++ blog/views.py
@@ -5,13 +5,13 @@ from django.http import JsonResponse, HttpResponse
 import json
 from django.forms.models import model_to_dict
 
+
 def post_list(request):
     posts = Post.published.all()
-    return render(request, 'blog/post/list.html', {'posts': posts})
-    
+    return render(request, "blog/post/list.html", {"posts": posts})
+
 
 def post_detail(request, id):
     post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     data = model_to_dict(post)
-    return render(request, 'blog/post/detail.html', {'post': post})
-    
+    return render(request, "blog/post/detail.html", {"post": post})
