05e1edfa9d274769db518c88824f09b00d6bf764
americobcn <americo.cot@gmail.com>
2025-02-23 18:10:29 +0100

    Added some styles

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.core import serializers
 from .models import Post
-from django.http import JsonResponse, HttpResponse
-import json
 from django.forms.models import model_to_dict
 
 
