1f4b33ae6954dd5ce996fc069da5204bb387d791
americobcn <americo.cot@gmail.com>
2025-02-27 15:47:24 +0100

    From Home: share styles

--- blog/forms.py
+++ blog/forms.py
@@ -3,10 +3,21 @@ from .models import Comment
 
 
 class EmailPostForm(forms.Form):
-    name = forms.CharField(max_length=25)
-    email = forms.EmailField()
-    to = forms.EmailField()
-    comments = forms.CharField(required=False, widget=forms.Textarea)
+    name = forms.CharField(max_length=25, widget=forms.TextInput(
+            attrs={
+                "class": "text-sm mx-2 my-3 px-2 max-w-xl border-2 border-amber-400 text-gray-100 focus:outline-none focus:border-amber-300 focus:border-4 focus:bg-gray-800 rounded-md "
+            }))
+    email = forms.EmailField(widget=forms.TextInput(
+            attrs={
+                "class": "text-sm mx-2 my-3 px-2 max-w-xl border-2 border-amber-400 text-gray-100 focus:outline-none focus:border-amber-300 focus:border-4 focus:bg-gray-800 rounded-md "
+            }))
+    to = forms.EmailField(widget=forms.TextInput(
+            attrs={
+                "class": "text-sm mx-2 my-3 px-2 max-w-xl border-2 border-amber-400 text-gray-100 focus:outline-none focus:border-amber-300 focus:border-4 focus:bg-gray-800 rounded-md "
+            }))
+    comments = forms.CharField(required=False, widget=forms.Textarea(attrs={
+                "class": "block text-sm mx-2 my-3 px-2 max-w-xl border-2 border-amber-400 text-gray-100 focus:outline-none focus:border-amber-300 focus:border-4 focus:bg-gray-800 rounded-md "
+            }))
 
 
 class CommentForm(forms.ModelForm):
