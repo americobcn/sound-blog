c05ef28db4909b412b15a4e129a7b8416225b725
americobcn <americo.cot@gmail.com>
2025-03-05 15:26:22 +0100

    Added vector search feature

--- blog/views.py
+++ blog/views.py
@@ -129,7 +129,7 @@ def post_search(request):
     if "query" in request.GET:
         form = SearchForm(request.GET)
         if form.is_valid():
-            query = form.changed_data["query"]
+            query = form.cleaned_data["query"]
             results = Post.published.annotate(
                 search=SearchVector("title", "body"),
             ).filter(search=query)
